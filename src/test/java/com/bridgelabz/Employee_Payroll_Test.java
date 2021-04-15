57445ab0d2ab5e3ad42eee19d48588a0c767f824
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-15 13:59:29 +0530

    [vinit]: Add given_sum_avg_min_max_count AND Add new Employee to employee_payroll table in payroll_service database

--- src/test/java/com/bridgelabz/Employee_Payroll_Test.java
+++ src/test/java/com/bridgelabz/Employee_Payroll_Test.java
@@ -7,27 +7,45 @@ import java.util.List;
 
 public class Employee_Payroll_Test {
     @Test
-    void givenEmployeePayroll_WhenRetrieved_ShouldMatchEmpCount() {
+    public void givenEmployeePayroll_WhenRetrieved_ShouldMatchEmpCount() {
         Employee_payroll employee_Payroll = new Employee_payroll();
         List<EmployeePayrollData> employeePayrollDataList= employee_Payroll.readData();
-        Assertions.assertEquals(3,employeePayrollDataList.size());
+        Assertions.assertEquals(4,employeePayrollDataList.size());
+
     }
 
     @Test
     public void givenNewSalaryForEmployee_whenUpdate_shouldSyncWithDB() {
         Employee_payroll employee_Payroll = new Employee_payroll();
-        employee_Payroll.updateData();
-        List<EmployeePayrollData> employeePayrollDataList=employee_Payroll.readData();
-        Assertions.assertEquals(3,employeePayrollDataList.size());
-        long result=employee_Payroll.updateData();
+        double salary=500000;
+        int id=3;
+        long result=employee_Payroll.updateData(id,salary);
         Assertions.assertEquals(1,result);
     }
 
     @Test
     public void givenDateRangeToEmployeePayRollInDB_WhenRetrieved_DataShouldMatchFilteredEmployeeCount() {
         Employee_payroll employee_Payroll = new Employee_payroll();
-        List<EmployeePayrollData>employeePayrollDataList=employee_Payroll.employeeDetailsfromDate();
-        Assertions.assertEquals(2,employeePayrollDataList.size());
+        String date = "2019-11-13";
+        List<EmployeePayrollData>employeePayrollDataList=employee_Payroll.employeeDetailsfromDate(date);
+        Assertions.assertEquals(3,employeePayrollDataList.size());
+    }
+    @Test
+    public void given_sum_avg_min_max_count() {
+        Employee_payroll employee_Payroll = new Employee_payroll();
+        List<String> list=employee_Payroll.Database_operation();
+        Assertions.assertEquals(12,list.size());
     }
 
+    @Test
+    public void insert_new_employee_in_employee_table(){
+        Employee_payroll employee_Payroll = new Employee_payroll();
+        String name="Sam";
+        String date="2021-07-07";
+        double salary=900000;
+        String gender="M";
+        employee_Payroll.AddDataInTable(name,date,salary,gender);
+        List<EmployeePayrollData> employeePayrollDataList=employee_Payroll.readData();
+        Assertions.assertEquals(4,employeePayrollDataList.size());
+    }
 }
